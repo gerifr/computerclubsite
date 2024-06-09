59be3c422bed207bf18779aef5aacf06b2d90c0d
GENIYAMA <geniusbhandari1@gmail.com>
2024-06-09 08:27:37 +0545

    added security to prevent users from logging in without veryfying, can't use periodic function to delete such user accounts cuz its locked in other price plans, hence we might either do it manually or provide forgot password option( one that I am going to do soon) in order to help  users log in even if someone else tried to faking their existence as the other person, Ik this is too mch yapping fr fr

--- js/logpage.js
+++ js/logpage.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
-import * as functions from "firebase-functions"; 
 const firebaseConfig = {
     apiKey: "AIzaSyAxCwtWGW4YLIgFBRHBdsTACrgbhjQm5o0",
     authDomain: "comptuerclubofarniko.firebaseapp.com",
@@ -17,31 +16,37 @@ const firebaseConfig = {
 
   const auth=getAuth();
   const db=getFirestore();
-
+  const homedisplay=document.getElementById('loghome');
   onAuthStateChanged(auth, (user)=>{
-    const loggedInUserId=localStorage.getItem('loggedInUserId');
-    if(loggedInUserId){
-        console.log(user);
-        const docRef = doc(db, "users", loggedInUserId);
-        getDoc(docRef)
-        .then((docSnap)=>{
-            if(docSnap.exists()){
-                const userData=docSnap.data();
-                document.getElementById('loggedUserFName').innerText=userData.firstName;
-                document.getElementById('loggedUserEmail').innerText=userData.email;
-                document.getElementById('loggedUserLName').innerText=userData.lastName;
-
-            }
-            else{
-                console.log("no document found matching id")
-            }
-        })
-        .catch((error)=>{
-            console.log("Error getting document");
-        })
+    if(user.emailVerified){
+        const loggedInUserId=localStorage.getItem('loggedInUserId');
+        if(loggedInUserId){
+            console.log(user);
+            const docRef = doc(db, "users", loggedInUserId);
+            getDoc(docRef)
+            .then((docSnap)=>{
+                if(docSnap.exists()){
+                    const userData=docSnap.data();
+                    document.getElementById('loggedUserFName').innerText=userData.firstName;
+                    document.getElementById('loggedUserEmail').innerText=userData.email;
+                    document.getElementById('loggedUserLName').innerText=userData.lastName;
+                    homedisplay.style.display="block";
+                }
+                else{
+                    console.log("no document found matching id")
+                }
+            })
+            .catch((error)=>{
+                console.log("Error getting document");
+            })
+        }
+        else{
+            console.log("User Id not Found in Local storage")
+        }
     }
     else{
-        console.log("User Id not Found in Local storage")
+        alert("Email isn't verified, you won't be given data mate");
+        window.location.href='index.html';
     }
   })
 
